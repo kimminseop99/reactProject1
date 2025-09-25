a4dc930109df109d5614095158557ec233ee674e
kimminseop99 <minseopkim99@gmail.com>
2025-09-25 16:28:50 +0900

    useEffect testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("run one time");
+  }, []);
+  useEffect(() => {
+    console.log("change counter");
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>hello world!</h1>   
-      <Button text={"Continue"}/> 
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+      <h1>counter = {counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
